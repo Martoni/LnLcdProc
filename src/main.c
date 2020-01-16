b35d254e36141a258650d692b440dba07bd6eb0a
Fabien Marteau <fabien.marteau@armadeus.com>
2020-01-16 22:11:01 +0100

    print on LCD event from usb-serial

--- src/main.c
+++ src/main.c
@@ -6,8 +6,34 @@
 */
 
 #include "gd32vf103.h"
+#include "drv_usb_hw.h"
+#include "cdc_acm_core.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lcd.h"
+
+/* USB part */
+extern uint8_t packet_sent, packet_receive;
+extern uint32_t receive_length;
+
+usb_core_driver USB_OTG_dev =
+{
+    .dev = {
+        .desc = {
+            .dev_desc       = (uint8_t *)&device_descriptor,
+            .config_desc    = (uint8_t *)&configuration_descriptor,
+            .strings        = usbd_strings,
+        }
+    }
+};
+
+// Write to USB CDC, reading should be similar
+void cdc_print (usb_dev *pudev, uint8_t *str, uint32_t data_len)
+{
+    packet_sent = 0;
+    usbd_ep_send(pudev, CDC_ACM_DATA_IN_EP, str, data_len);
+}
 
 /* RGB LED is connected as follows:
     PC13 : Red
@@ -118,21 +144,57 @@ int main(void)
     green_off();
     blue_off();
     LCD_Clear(RED);
+
+    uint8_t strin[100] = "Test\r\n";
+    int i = 0;
+    eclic_global_interrupt_enable();
+    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL2_PRIO2);
+    usb_rcu_config();
+    usb_timer_init();
+    usb_intr_config();
+    usbd_init (&USB_OTG_dev, USB_CORE_ENUM_FS, &usbd_cdc_cb);
+    /* check if USB device is enumerated successfully */
+    while (USBD_CONFIGURED != USB_OTG_dev.dev.cur_status) {
+    }
+
+    LCD_Clear(RED);
+    green_on();
+
     while(1){
-        red_on();
-        LCD_Clear(RED);
-        delay_1ms(100);
-        red_off();
-        delay_1ms(100);
-        green_on();
-        LCD_Clear(GREEN);
-        delay_1ms(100);
-        green_off();
-        delay_1ms(100);
-        blue_on();
-        LCD_Clear(BLUE);
-        delay_1ms(100);
-        blue_off();
-        delay_1ms(100);
+        if (USBD_CONFIGURED == USB_OTG_dev.dev.cur_status) {
+            if (1 == packet_receive && 1 == packet_sent) {
+                packet_sent = 0;
+                /* receive datas from the host when the last packet datas have sent to the host */
+                cdc_acm_data_receive(&USB_OTG_dev);
+            } else {
+                if (0 != receive_length) {
+                    i++;
+                    /* send receive datas */
+                    sprintf((char*)strin, "Test %d\r\n",i);
+		    /* print receive data on LCD */
+		    LCD_ShowString(24,  0, strin, BLACK);
+                    cdc_print(&USB_OTG_dev, strin, strlen((char*)strin)); //Test for printing a string
+                    //cdc_acm_data_send(&USB_OTG_dev, receive_length); (Original example with simple loopback)
+                    receive_length = 0;
+                }
+            }
+        }
+//        red_on();
+//        LCD_Clear(RED);
+//        delay_1ms(100);
+//        red_off();
+//        delay_1ms(100);
+//        green_on();
+//        LCD_Clear(GREEN);
+//        delay_1ms(100);
+//        green_off();
+//        delay_1ms(100);
+//        blue_on();
+//        LCD_Clear(BLUE);
+//        delay_1ms(100);
+//        blue_off();
+//        delay_1ms(100);
     }
 }
+
+
